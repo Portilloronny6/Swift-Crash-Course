1d66b3c91785365cafbe17120f06aa2eea732964
Ronny Portillo <portilloronny6@gmail.com>
2023-10-13 00:34:52 +0500

    optionals

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -2,15 +2,19 @@ import Foundation
 
 // Cuando existe la posibilidad de que haya una ausencia de valor, el tipo de la variable sera opcional.
 
+
+// "Siempre habra un valor y ese valor será String"
 let myStringNumber: String = "123"
-let myNumber: Int? = Int(myStringNumber)
+
+// "No sabemos si habrá un valor, pero en caso de haberlo, será Int.
+let myNumber: Int? = Int(myStringNumber) // podría ser nil.
 
 print(myNumber!)
 
 let myString: String = "Hola"
 let myOptionalInt = Int(myString)
 
-// el operador ?? me permite chequear si el valor de una variable es 'nil' y en caso de serlo, devolvera un valor por defecto.
+// el operador ?? (nil coalescing operator) me permite chequear si el valor de una variable es 'nil' y en caso de serlo, devolverá un valor por defecto.
 
 print(myOptionalInt ?? "Привет")
 
@@ -33,6 +37,122 @@ myNumber.map { value in
 }
 
 
+let newValueBool: Bool? = true
+
+// Asignaré el valor de newValueBool (si es que tiene alguno), de lo contrario sera falso
+let newValueBool2: Bool = newValueBool ?? false
+
+print("New value 2: \(newValueBool2.description)")
 
 
+func checkIfUserIsPremium(userIsPremium: Bool?) -> Bool {
+    
+    // If there is a value, let newValue equal that value
+    if let newValue = userIsPremium {
+        // Here we have access to the non-optional value
+        return newValue
+    }
+    
+    return false
+}
 
+func checkIfUserIsPremium2(userIsPremium: Bool?) -> Bool {
+    
+    // If there is a value, let newValue equal that value
+    if let userIsPremium {
+        // Here we have access to the non-optional value
+        return userIsPremium
+    }
+    
+    return false
+}
+
+// guard
+
+func checkIfUserIsPremium3(userIsPremium: Bool?) -> Bool {
+    
+    // Make sure there is a value, otherwise, return out of the function
+    guard let newValue = userIsPremium else {
+        return false
+    }
+    
+    // here we have access to the non-optional value
+    return newValue
+}
+
+
+func checkIfUserIsPremium4(userIsPremium: Bool?) -> Bool {
+    
+    // Make sure there is a value, otherwise, return out of the function
+    guard let userIsPremium else {
+        return false
+    }
+    
+    // here we have access to the non-optional value
+    return userIsPremium
+}
+
+var userIsNew: Bool? = true
+var userDidCompleteOnboarding: Bool? = false
+var userFavoriteMovie: String? = nil
+
+func getUserStatus(userIsNew: Bool, userDidCompleteOnboarding: Bool, userFavoriteMovie: String) -> Bool {
+    if !userIsNew || !userDidCompleteOnboarding {
+        return false
+    }
+    
+    return true
+}
+
+func checkIfUserIsSetup(userIsNew: Bool?, userDidCompleteOnboarding: Bool?, userFavoriteMovie: String?) -> Bool {
+    
+    if let userIsNew, let userDidCompleteOnboarding, let userFavoriteMovie {
+        return getUserStatus(
+            userIsNew: userIsNew,
+            userDidCompleteOnboarding: userDidCompleteOnboarding,
+            userFavoriteMovie: userFavoriteMovie
+        )
+    }
+    
+    return false
+}
+
+func checkIfUserIsSetup2(userIsNew: Bool?, userDidCompleteOnboarding: Bool?, userFavoriteMovie: String?) -> Bool {
+    
+    guard let userIsNew, let userDidCompleteOnboarding, let userFavoriteMovie else {
+        return false
+    }
+    
+    return getUserStatus(
+        userIsNew: userIsNew,
+        userDidCompleteOnboarding: userDidCompleteOnboarding,
+        userFavoriteMovie: userFavoriteMovie
+    )
+}
+
+
+func getUsername() -> String? {
+    return "Engiber"
+}
+
+func getTitle() -> String {
+    return "Swift"
+}
+
+func getUserData() {
+    
+    let username: String? = getUsername()
+    
+    // I will get the count if the username is not nil
+//    let count: Int? = username?.count
+    
+    let title: String = getTitle()
+    
+    // I will get the count always
+    let count2 = title.count
+    
+    // If username has a value, and first character in username has a value, then return the value of isLowercase
+    // Optional chaining
+    let _: Bool = username?.first?.isLowercase ?? false
+    
+}
